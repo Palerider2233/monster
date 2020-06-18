b06ae7bec5067fd698e4c7a9f2c2cb68323708b9
Palerider2233 <61342186+Palerider2233@users.noreply.github.com>
2020-06-18 21:06:48 +0700

    ada

---
+++ Assets/Scripts/Generate Enemies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratEenemies : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public ini xPos;
+    public ini zPos;
+    public ini enemyCount;
+
+    void Start ();
+    {
+        
+    }
+    IEnumerator EnermyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(1,50);
+            zPos = Random.Range(1,31);
+            Instaniate(theEnemy,new Vector3(xPos,0,zPos),Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount +=1;
+        }
+    }
+}
